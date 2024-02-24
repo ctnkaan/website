7e24c829ac1ed5f2ee0ee0ce4b475ddb5730d623
ctnkaan <kaan.taskingenc@gmail.com>
2024-02-25 01:00:46 +0300

    feat: add home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,28 @@ function Home() {
       <h1>Çetin Kaan Taşkıngenç</h1>
       <p>
         Welcome to my public website.
-        Insted of an terminal website like the one I had before, I wanted to craft something that is minimal, efortless to add content to and easily readable by everyone, 
-        thus this website was born.
-        </p>
-      <h2>About</h2>
-      <p>
-        Aliquam vestibulum tellus sit amet est consectetur convallis. 
-        Nulla sed nisl vitae neque tempus dictum sed et nisi. 
-        In interdum lorem ut ipsum sollicitudin, id auctor turpis gravida. 
-        Duis id dictum felis, eu dictum odio. Donec pellentesque pharetra nibh at pellentesque. 
-        In ullamcorper purus lobortis, ultricies tellus sed, tincidunt diam. Sed ex nisl, consectetur et ultrices quis, dignissim at eros. 
-        Proin ut quam pharetra, aliquam ligula sed, mattis dolor. Donec vel sollicitudin nulla, eget mollis nulla. 
-        Maecenas placerat, neque sit amet condimentum auctor, ligula purus fringilla mi, quis efficitur dolor augue id quam. Cras feugiat, nisl vitae venenatis volutpat, lorem ipsum rhoncus erat, eget tincidunt mi lectus et orci. 
-        Praesent placerat eleifend quam, ac consequat libero accumsan non. Integer sit amet est id justo interdum cursus. 
-        Donec a libero eget sem pellentesque porttitor vel vitae augue. 
+        Insted of an terminal website like the one I had before, I wanted to craft something that is minimal,
+        efortless to add content to and easily readable by everyone, thus this website was born.
       </p>
+      <h2>About</h2>
+
+      <p>
+        I majored in Management Information Systems and discovered my love for software craftmanship here.
+        I practiced and basicaly thought myself software development online. 
+        I loved the field so much I even minored in Computer Science.  
+      </p>
+
+      <p>
+        In my 4+ years, I found my love for communities and teaching. 
+        Spoke at 30+ tech events, joined cool groups like Microsoft Learn Student Ambassadors, Google Developer Student Clubs, Postman Student Leaders, and IEEE Computer Society.
+        Ended up as the 2nd Gold Microsoft Student Ambassador and Turkey Lead. Despite being an introvert, I picked up some awesome people skills through these communities.
+      </p>
+
+      <p>
+      Post-Uni, I delved into web app development at MindWorks for 1.5 years. Wanted more experience, so I left to join a larger software dev team.
+      Now, I'm with MagiClick, rocking it in the React Team.
+      </p>
+
       <Footer />
     </>
   )
