1e58ceb1303dcacceb35891fe7447fd610eaf0bf
ctnkaan <kaan.taskingenc@gmail.com>
2024-04-13 01:06:06 +0300

    refactor: projects rendering

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,30 @@
 import Project from "../components/Project"
 
+const projects = [
+  {
+    name: "Postman Sentinel",
+    url: "https://github.com/ctnkaan/Postman-Sentinel",
+    desc: "A Cybersecurity Discord Bot that has blocked 1000+ phishing attacks"
+  },
+  {
+    name: "Terminal Website",
+    url: "https://github.com/ctnkaan/portfolio",
+    desc: "My old website themed around an unix terminal"
+  }
+]
+
 const Projects = () => {
   return (
     <div>
         <h1>Projects</h1>
 
-        <Project 
-          name={"Postman Sentinel"} 
-          url={"https://github.com/ctnkaan/Postman-Sentinel"} 
-          desc={"A Cybersecurity Discord Bot that has blocked 1000+ phishing attacks"}
-        />
-        <Project name={"Terminal Website"} url={"https://github.com/ctnkaan/portfolio"} desc={"My old website themed around an unix terminal"}/>
+        {projects.map((project) => (
+          <Project
+            name={project.name}
+            url={project.url}
+            desc={project.desc}
+          />
+        ))}
     </div>
   )
 }
