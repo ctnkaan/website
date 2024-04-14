f28717304133668108d7b2130bca1aa40851d0fd
ctnkaan <kaan.taskingenc@gmail.com>
2024-04-14 22:44:10 +0300

    refactor: move svgs to public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,32 @@
-import "../styles/Footer.css"
+import '../styles/Footer.css'
 
 const Footer = () => {
   return (
-    <div className="footer">
-        <hr />
-        <div className="innerFooter">
-          <div className="footerIcons">
-              <a href="https://www.github.com/ctnkaan"><img src="/src/assets/github.svg" className="icon"/> </a>
-              <a href="https://www.linkedin.com/in/cetinkaantaskingenc/"><img src="/src/assets/linkedin.svg" className="icon" /> </a>
-              <a href="https://www.instagram.com/cetinkaantaskingenc"><img src="/src/assets/instagram.svg" className="icon" /> </a>
-              <a href="https://www.twitter.com/cetinkaantweets"> <img src="/src/assets/twitter-circled.svg" className="icon" /> </a>
-              <a href="https://www.youtube.com/@CetinKaanTaskingenc"> <img src="/src/assets/youtube.svg" className="icon" /> </a>
-          </div>
-
-          <div>
-            <p>&copy 2024 </p>
-          </div>
-
+    <div className='footer'>
+      <hr />
+      <div className='innerFooter'>
+        <div className='footerIcons'>
+          <a href='https://www.github.com/ctnkaan'>
+            <img src='assets/github.svg' className='icon' />
+          </a>
+          <a href='https://www.linkedin.com/in/cetinkaantaskingenc/'>
+            <img src='assets/linkedin.svg' className='icon' />
+          </a>
+          <a href='https://www.instagram.com/cetinkaantaskingenc'>
+            <img src='assets/instagram.svg' className='icon' />
+          </a>
+          <a href='https://www.twitter.com/cetinkaantweets'>
+            <img src='assets/twitter-circled.svg' className='icon' />
+          </a>
+          <a href='https://www.youtube.com/@CetinKaanTaskingenc'>
+            <img src='assets/youtube.svg' className='icon' />
+          </a>
         </div>
+
+        <div>
+          <p>&copy 2024 </p>
+        </div>
+      </div>
     </div>
   )
 }
