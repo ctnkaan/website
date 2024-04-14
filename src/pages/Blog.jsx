b640444051910abbc00694e760c2052ee4f4d742
ctnkaan <kaan.taskingenc@gmail.com>
2024-04-14 21:12:33 +0300

    feat: add blog section

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,15 @@
+import blogs from '../data/blog/blogs.json'
+import Card from '../components/Card'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div>
+      <h1>Blog</h1>
+
+      {blogs.map(project => (
+        <Card name={project.name} url={project.url} desc={project.desc} />
+      ))}
+    </div>
   )
 }
 
