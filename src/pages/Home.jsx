fe45dafcd57bca1c0ffee0dab70f63f751012ac9
Çetin Kaan Taşkıngenç <kaan.taskingenc@gmail.com>
2024-06-07 09:56:04 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,24 +12,25 @@ function Home() {
 
       <p>
         I majored in Management Information Systems and discovered my love for
-        software craftmanship here. I practiced and basicaly thought myself
-        software development online. I loved the field so much I even minored in
-        Computer Science.
+        software craftmanship here. I thought myself software development thanks 
+        to great online resources like CS50. I also even minored in Computer Science
+        during my time in university. Even became a teaching assistant.
       </p>
 
       <p>
-        In my 4+ years, I found my love for communities and teaching. Spoke at
-        30+ tech events, joined cool groups like Microsoft Learn Student
+        I found my love for communities and teaching during studying. 
+        Spoke at 9 tech events and hosted 17 technical workshops, 
+        got accepted to developer advocate groups like Microsoft Learn Student
         Ambassadors, Google Developer Student Clubs, Postman Student Leaders,
         and IEEE Computer Society. Ended up as the 2nd Gold Microsoft Student
-        Ambassador and Turkey Lead. Despite being an introvert, I picked up some
-        awesome people skills through these communities.
+        Ambassador and Turkey Lead at the end. I picked up some
+        awesome people skills through these communities and met with wonderful people.
       </p>
 
       <p>
-        Post-Uni, I delved into web app development at MindWorks for 1.5 years.
-        Wanted more experience, so I left to join a larger software dev team.
-        Now, I'm with MagiClick, rocking it in the React Team.
+        Post-Uni, I delved into web app development at MindWorks. I stayed there for 1.5 years.
+        At the end I wanted more experience at a larger team, so I accepted an offer join a larger 
+        software dev team. Currently I'm in the React team of MagiClick.
       </p>
     </>
   )
